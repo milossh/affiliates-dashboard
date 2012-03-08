015a94fb354304acca3558aeeb584d284bd5da7e
Milos Dinic <milos@mozilla.com>
2012-03-08 23:16:32 +0100

    new code changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-/* Authors: Matjaž Horvat
-            Milos Dinic
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is triage helper.
+ *
+ * The Initial Developer of the Original Code is Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):  Milos Dinic <mdinic@mozilla.com>
+ *                  Matjaz Horvat <mhorvat@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
-*/
 
 $(function() {
 
@@ -10,9 +42,7 @@ $(function() {
   function render() {
     for (var locale in locales) {
       var ids = [];
-      $(locales[locale]).each(function() {
-        ids.push(this.id);
-      });
+      ids.push(locales.buglist[0]);
       $('table').append(
         '<tr class="' + locale + '"><td>' + 
         '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + ids.join(', ') + '">' + locale + '</a>' + 
@@ -26,15 +56,29 @@ $(function() {
 
   function parse(summary) {
     var meta = {};
+
+    /*  We get something like:
+          [sr][mobile][thunderbird] Please fix this bug ASAP"
+        First we split it by whitespace, so that we get what we call
+        parameters([sr][mobile][thunderbird]) and summary(Please fix this bug ASAP).
+    */
     temp = summary.split(" ");
     parameters = temp[0];
-    meta.summary = temp.slice(1).join(" ");
+    meta.summary = temp.slice(1).join(" "); /*  Take everything except first part
+                                                and add whitespaces again, getting
+                                                the summary we want.
+                                            */
     var params = [];
-    $(parameters.split("[")).each(function() {
-      var tempParams = this.split("]");
+    $.each(parameters.split("["), function(index, value) {
+      var tempParams = this.split("]");  /* Split everything by "[" and "]" after,
+                                            so that we get an array of parameters
+                                            ie. ['sr', 'mobile', 'thunderbird'] 
+                                          */
       for (var i = 0; i < tempParams.length; i++) {
         if (tempParams[i] == "") {        
-          tempParams.splice(i, 1);
+          tempParams.splice(i, 1);        /*  Check if parameter has value, and remove it
+                                              if it doesn't.
+                                          */
           i--;
         }
       }
@@ -64,6 +108,9 @@ $(function() {
     }
 
     if (!locales[meta.locale][bug.id]) {
+      locales[meta.locale].buglist = [];
+      locales[meta.locale].buglist.push(bug.id);
+      console.log("just added to locale " + meta.locale + " a bug number " + bug.id);
       locales[meta.locale][bug.id] = bug.id;
     } else {
       locales[meta.locale][bug.id].push(bug.id);
@@ -79,7 +126,7 @@ $(function() {
         Response contains the bug with all the properties
     */
     
-    url: 'https://api-dev.bugzilla.mozilla.org/0.9/bug', 
+    url: 'https://api-dev.bugzilla.mozilla.org/1.1/bug', 
     dataType: 'json',
     data: {
       product: 'Websites',
@@ -87,12 +134,10 @@ $(function() {
     }, 
     success: function(data) {
       /* if we get successful fetch call add() for each bug in response */
-      $(data.bugs).each(function() {
+      $.each(data.bugs, function(index, value) {
         add(this);
       });
       render();
     }
   });
 });
-
-
